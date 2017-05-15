d4ee2e58fb5d4af51790a4238533eff16170cf9d
Katharina-Maria Heer <kathy11997@gmx.de>
2017-05-15 20:47:27 +0200

    removing semicolons, adding line breaks

--- src/Customer.java
+++ src/Customer.java
@@ -3,26 +3,34 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
+
     private String name;
     private Vector rentals = new Vector();
-    public Customer (String newname){
+    public Customer (String newname)
+    {
         name = newname;
-    };
-    public void addRental(Rental arg) {
+    }
+
+    public void addRental(Rental arg)
+    {
         rentals.addElement(arg);
-    };
-    public String getName (){
+    }
+
+    public String getName ()
+    {
         return name;
-    };
-    public String statement() {
+    }
+    public String statement()
+    {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
+        while (enum_rentals.hasMoreElements())
+        {
+            double thisAmount;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
             thisAmount = amountFor(each);
@@ -41,9 +49,11 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental each)
+    {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (each.getMovie().getPriceCode())
+        {
             case Movie.REGULAR:
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
