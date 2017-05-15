2d9763107b2ed83a54a553b46d7fc7d26854f9b3
Katharina-Maria Heer <kathy11997@gmx.de>
2017-05-15 22:09:09 +0200

    Move methode getFrequentRenterPoints from Movie to Price

--- src/Movie.java
+++ src/Movie.java
@@ -68,13 +68,6 @@ public class Movie
     }
     int getFrequentRenterPoints(int days)
     {
-        if (getPriceCode() == Movie.NEW_RELEASE && days > 1)
-        {
-            return 2;
-        }
-        else
-        {
-            return 1;
-        }
+        return this.price.getFrequentRenterPoints(days);
     }
 }
--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -1,13 +1,22 @@
 class NewReleasePrice extends Price {
 
     @Override
-    public double getCharge(int days) {
+    public double getCharge(int days)
+    {
         return days * 3;
     }
 
     @Override
-    public int getPriceCode() {
+    public int getPriceCode()
+    {
         return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    int getFrequentRenterPoints(int days)
+    {
+        if (days > 1) return 2;
+        return super.getFrequentRenterPoints(days);
     }
 
 
