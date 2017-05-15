52046ca1f79f177430d96cd61928b2c96394991e
Katharina-Maria Heer <kathy11997@gmx.de>
2017-05-15 21:34:15 +0200

    Create method getTotalCharge

--- src/Customer.java
+++ src/Customer.java
@@ -25,7 +25,6 @@ class Customer
 
     public String statement()
     {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
@@ -38,11 +37,22 @@ class Customer
 
             //show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-            totalAmount += rental.getCharge();
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + getTotalCharge() + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge()
+    {
+        double result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements())
+        {
+            Rental rental = (Rental) rentals.nextElement();
+            result += rental.getCharge();
+        }
         return result;
     }
 
